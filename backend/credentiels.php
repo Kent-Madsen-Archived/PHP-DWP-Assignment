f49d537ccc36fcab6e2863341732892e2a17c0f8
Kent vejrup Madsen <kent.vejrup.madsen@outlook.com>
2020-09-07 10:57:14 +0200

    current

---
+++ backend/credentiels.php
@@ -0,0 +1,5 @@
+<?php 
+
+header("Location: ../index.php");
+echo "Backend file.";
+?>
--- login.php
+++ login.php
@@ -1,23 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-       
-        <link rel="stylesheet" 
-              href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
-              integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
-              crossorigin="anonymous">
+    <?php include_once './areas/head.php'; ?>
 
-        <title>
-            <?php echo 'Login - DWP PHP Assignment'; ?>
-        </title>
-    </head>
     <body>
         <?php include_once './areas/header.php'; ?>
     
         <main> 
-            <form class="form-signin"> 
+            <form class="form-signin" method="post" action="./backend/credentiels.php"> 
                 <h1> Login </h1>
                 <p> Please type in your login information. </p>
                 
