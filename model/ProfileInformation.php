5486f86b7d4a5ceece20c633f212d71d1ce4b5c2
Kent vejrup Madsen <kent.vejrup.madsen@outlook.com>
2020-09-10 10:05:24 +0200

    get / setters

--- model/ProfileInformation.php
+++ model/ProfileInformation.php
@@ -14,5 +14,47 @@ class ProfileInformation
     private $postZone;
     private $country;
     private $birthday;
+
+            
+    function getIdentity()
+    {
+        return $this->identity;
+    }
+
+    function setIdentity($var)
+    {
+        $this->identity = $var;
+    }
+
+    function getProfileIdentity()
+    {
+        return $this->profileIdentity;
+    }
+
+    function setProfileIdentity($var)
+    {
+        $this->profileIdentity = $var;
+    }
+
+
+    function getPersonName()
+    {
+        return $this->personName;
+    }
+
+    function setPersonName($var)
+    {
+        $this->personName = $var;
+    }
+
+    function getAddress()
+    {
+        return $this->address;
+    }
+
+    function setAddress($var)
+    {
+        $this->address = $var;
+    }
 }
 ?>
