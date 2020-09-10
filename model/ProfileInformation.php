e13b6fc831aff2599f84e73dc4cda0527a48934e
Kent vejrup Madsen <kent.vejrup.madsen@outlook.com>
2020-09-10 10:00:12 +0200

    current

--- model/ProfileInformation.php
+++ model/ProfileInformation.php
@@ -1,3 +1,18 @@
 <?php 
+class ProfileInformation
+{
+    public function __construct() 
+    {
+    
+    }
 
+    private $identity;
+    private $profileIdentity;
+
+    private $personName;
+    private $address;
+    private $postZone;
+    private $country;
+    private $birthday;
+}
 ?>
