3b0572e3e568b7f0c02b33049cbc7162e3aef553
Kent vejrup Madsen <kent.vejrup.madsen@outlook.com>
2020-09-11 18:46:26 +0200

    added bootstrap

--- login.php
+++ login.php
@@ -4,12 +4,32 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Login</title>
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
         <header> </header>
         <main> 
-        
-        
+            <form id="login" method="post" action="./login.php"> 
+                <h1> Login Form </h1>
+                
+                <span> 
+                    <input class="input-area" type="text" name="form_username" placeholder="username">
+                </span>
+                
+                <span>
+                    <input class="input-area" type="password" name="form_password" placeholder="password">
+                </span>
+
+                <span>
+                <!-- Forgot password? -->
+                    <p> </p>
+
+                </span>
+                
+                <span>
+                    <input class="button" type="submit" value="login">
+                </span>
+            </form>
         </main>
         <footer> </footer>
     </body>
