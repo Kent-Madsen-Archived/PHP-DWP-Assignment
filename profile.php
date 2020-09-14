c68ad764dbd35fd0858e638f3dc6decebbb5f352
Kent vejrup Madsen <kent.vejrup.madsen@outlook.com>
2020-09-14 10:29:16 +0200

    a

--- profile.php
+++ profile.php
@@ -20,6 +20,7 @@ $allMails = $mailFactory->findAllByProfileIdentity($_SESSION["id"]);
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>My Profile</title>
+        <link rel="stylesheet" href="style.css">
     </head>
     
     <body>
