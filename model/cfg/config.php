c9995cc3c87483aa467c9a8876bd25a29d4383fc
Kent vejrup Madsen <kent.vejrup.madsen@outlook.com>
2020-09-16 08:21:18 +0200

    i'll do the website for pyromaniacs instead

--- model/cfg/config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-const database_server_name = "localhost";
-const database_username_name = "root";
-const database_password_name = "";
-const database_database_name = "dwp_assignment";
-
-
-?>
