f54bfee609855a3a0133d5741c00b1ab7fdd7971
Kent vejrup Madsen <kent.vejrup.madsen@outlook.com>
2020-09-17 15:54:24 +0200

    current

--- login.php
+++ login.php
@@ -1,3 +1,23 @@
+<?php require 'main.php'; ?>
 <?php 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <main> 
+            <form action="login_form.php" method="post">
+                <input type="text" placeholder="username" name="username_form"> 
+                <input type="password" placeholder="password" name="password_form"> 
+                
+                <input type="submit" value="Login User">
+            </form>
+        </main>
+    </body>
+</html>
--- register.php
+++ register.php
@@ -1,7 +1,24 @@
-<?php 
+<?php require 'main.php'; ?>
 
-include 'model/profileFactory.php';
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <main> 
+            <form action="register_form.php" method="post">
+                <input type="text" placeholder="username" name="username_form"> 
+                
+                <input type="text" placeholder="e-mail" name="email_form"> 
 
-$factory = new ProfileFactory;
-
-?>
+                <input type="password" placeholder="password" name="password1_form">
+                <input type="password" placeholder="password" name="password2_form"> 
+                
+                <input type="submit" value="Register Username">
+            </form>
+        </main>
+    </body>
+</html>
