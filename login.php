f8072fd38e79c74283bd31819585f00201873bfb
Kent vejrup Madsen <kent.vejrup.madsen@outlook.com>
2020-09-24 21:36:32 +0200

    a

--- login.php
+++ login.php
@@ -1,4 +1,30 @@
 <?php require_once 'head.php'; ?>
+
+<?php if( isset( $_POST[ 'login' ] ) == true ): ?>
+    <?php 
+    $connection = new mysqli('localhost', 'root', '', 'dwp_assignment');
+    
+    $sql = "select identity, username, password, email from profile where username='". $_POST["username_input"] ."';";
+    $result = $connection->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) 
+        {
+            if( password_verify( $_POST['password_input'], $row['password'] ) )
+            {
+                // Congratulations, you're logged in.
+            }
+        }
+      } else {
+        echo "0 results";
+      }
+
+
+    $connection->close();
+    ?>
+<?php endif; ?>
+
 <html lang="en">
     <head>
         <meta charset="UTF-8">
