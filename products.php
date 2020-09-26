8e5a60ca487468d00c9f288ae5dc3cbae66be1be
Kent vejrup Madsen <kent.vejrup.madsen@outlook.com>
2020-09-26 13:40:44 +0200

    products page has been made

--- products.php
+++ products.php
@@ -12,6 +12,27 @@
     <body>
         <?php require 'header.php'; ?>  
         <main>
+        <?php 
+        $connection = new mysqli('localhost', 'root', '', 'dwp_assignment');
+
+        $sql = "SELECT * FROM product_view;";
+        $result = $connection->query($sql);?>
+
+<?php
+        if ( $result->num_rows > 0 ): ?>
+            <?php while($row = $result->fetch_assoc()): ?>
+            <a href="product.php?identity=<?php echo $row["identity"]; ?>"> 
+                <p> <?php echo $row["title"] ?> </p>
+                <p> <?php echo $row["product_category"] ?> </p>
+                <p> <?php echo $row["description"] ?> </p>
+            </a>
+
+            <?php endwhile; ?>
+        <?php endif; ?>
+
+        <?php
+            $connection->close();
+        ?>
                 
         </main>
         <?php require 'footer.php'; ?>  
