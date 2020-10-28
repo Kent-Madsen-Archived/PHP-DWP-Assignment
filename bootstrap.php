df9b15218b318514a01fa7c92e16f7865e3a6e54
Kent vejrup Madsen <Kent.vejrup.madsen@outlook.com>
2020-10-28 19:06:07 +0100

    can have multiple directories in library now. but. class is still loaded by the class name, with php as extension

--- bootstrap.php
+++ bootstrap.php
@@ -5,14 +5,18 @@
     //
     require 'functions.php';
 
+    //
+    require 'autoloader.php';
+
     // Autoloader script for loading classes automaticly.
     // So i don't have to require the files
     spl_autoload_register(
         function ( $class_name ) 
         {
+            $autoloader = new Autoloader( 'lib/' );
+            $autoloader->load();
             
-            echo "class:" . $class_name . "</br>";
-            include "lib/" . $class_name . '.php';
+            $autoloader->extract_class( $class_name );
         }
     );
 ?>
