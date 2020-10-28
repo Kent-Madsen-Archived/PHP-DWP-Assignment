aa15fc2c4c535e131248cf3337766542b0e13cec
Kent vejrup Madsen <Kent.vejrup.madsen@outlook.com>
2020-10-28 17:29:21 +0100

    current

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php 
+    /**
+     * 
+     */
+    session_start();
+?>
+
+<?php 
+    // Internal Libraries
     require_once 'bootstrap.php'; 
     require_once 'router.php'; 
 
+    // Variables
     $router = new Router();
 
+    // Adds predfined routes, to the router.
     $router->appendRoutes( new Route( 'homepage', 'views/index.php' ) );
     $router->appendRoutes( new Route( 'setup', 'views/setup.php' ) );
     
@@ -15,11 +25,12 @@
 
     $router->appendRoutes( new Route( 'profile', 'views/profile.php' ) );
 
-    // 
+    // Special Routes, like page 404.
     $special_404 = new Route( '404', 'views/404.php' );
     $router->setSpecialPage404( $special_404 );
     $router->appendRoutes( $special_404 );
 
-    //
+
+    // Load current page or domain
     $router->load_view();
 ?>
