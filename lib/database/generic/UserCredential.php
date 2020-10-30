99e529e83b7ac77cefcb47537578830ff15bd37e
Kent vejrup Madsen <Kent.vejrup.madsen@outlook.com>
2020-10-30 12:26:33 +0100

    current

--- lib/database/generic/UserCredential.php
+++ lib/database/generic/UserCredential.php
@@ -23,7 +23,7 @@
         /**
          * 
          */
-        public function getUsername()
+        final public function getUsername()
         {
             return $this->username;
         }
@@ -31,7 +31,7 @@
         /**
          * 
          */
-        public function getPassword()
+        final public function getPassword()
         {
             return $this->password;
         }
@@ -39,7 +39,7 @@
         /**
          * 
          */
-        public function setUsername( $var )
+        final public function setUsername( $var )
         {
             $this->username = $var;
         }
@@ -47,7 +47,7 @@
         /**
          * 
          */
-        public function setPassword( $var )
+        final public function setPassword( $var )
         {
             $this->password = $var;
         }
