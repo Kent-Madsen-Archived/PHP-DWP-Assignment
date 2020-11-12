404bb01b3b7b71a0bb3bc2ed2f4327faaabfd9a1
Kent vejrup Madsen <Kent.vejrup.madsen@outlook.com>
2020-11-12 12:32:33 +0100

    added accessors to brought product model

--- lib/model/model/BroughtProductModel.php
+++ lib/model/model/BroughtProductModel.php
@@ -18,17 +18,107 @@
         /**
          * 
          */
+        public function setProductId( $var )
+        {
+            $this->product_id = $var;
+        }
+
+
+        /**
+         * 
+         */
+        public function getProductId()
+        {
+            return $this->product_id;
+        }
+        
+
+        /**
+         * 
+         */
+        public function setPrice( $var )
+        {
+            $this->price = $var;
+        }
+
+        /**
+         * 
+         */
+        public function getPrice()
+        {
+            return $this->price;
+        }
+        
+
+        /**
+         * 
+         */
+        public function getNumberOfProducts()
+        {
+            return $this->number_of_products;
+        }
+
+
+        /**
+         * 
+         */
+        public function setNumberOfProducts( $var )
+        {
+            $this->identity = $number_of_products;
+        }
+
+
+        /**
+         * 
+         */
         public function setIdentity( $var )
         {
             $this->identity = $var;
         }
-        
+
+
         /**
          * 
          */
         public function getIdentity()
         {
             return $this->identity;
+        }
+        
+
+        /**
+         * 
+         */
+        public function setInvoiceId( $var )
+        {
+            $this->invoice_id = $var;
+        }
+
+
+        /**
+         * 
+         */
+        public function getInvoiceId()
+        {
+            return $this->invoice_id;
+        }
+        
+
+        /**
+         * 
+         */
+        public function setRegistered( $var )
+        {
+            $this->registered = $var;
+        }
+
+
+        /**
+         * 
+         */
+        public function getRegistered()
+        {
+            return $this->registered;
         }
 
     }
