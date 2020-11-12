ba4e719a3483609656b88488900eefea35dd3acc
Kent vejrup Madsen <Kent.vejrup.madsen@outlook.com>
2020-11-12 16:34:28 +0100

    updating code

--- lib/factory/PersonEmailFactory.php
+++ lib/factory/PersonEmailFactory.php
@@ -95,11 +95,26 @@
                 throw new Exception( 'Error: ' . $connection->connect_error );
             }
 
+            $sql = "SELECT * FROM person_email LIMIT ? OFFSET ?;";
+
+            $stmt_limit = null;
+            $stmt_offset = null;
+
             try
             {
-                $sql = "select * from person_email;";
+                $stmt = $connection->prepare( $sql );
 
-                $result = $connection->query( $sql );
+                $stmt->bind_param( "ii",
+                                    $stmt_limit,
+                                    $stmt_offset );
+
+                $stmt_limit = $this->getLimit();
+                $stmt_offset = $this->calculateOffset();
+
+                // Executes the query
+                $stmt->execute();
+
+                $result = $stmt->get_result();
 
                 if( $result->num_rows > 0 )
                 {
@@ -150,7 +165,9 @@
             {
                 $stmt = $connection->prepare( $sql );
 
-                $stmt->bind_param( "s", $email );
+                $stmt->bind_param( "s",
+                                    $email );
+
                 $stmt->execute();
 
                 $result = $stmt->get_result();
@@ -186,7 +203,55 @@
          */
         final public function update( $model )
         {
+            $retVal = false;
 
+            $this->getConnector()->connect();
+
+            $connection = $this->getConnector()->getConnector();
+
+            if( $connection->connect_error )
+            {
+                throw new Exception( 'Error: ' . $connection->connect_error );
+            }
+
+            $sql = "UPDATE person_email SET content = ? WHERE identity = ?;";
+
+            try
+            {
+                $stmt = $connection->prepare( $sql );
+
+                //
+                $stmt->bind_param( "si",
+                                    $stmt_email,
+                                    $stmt_identity );
+
+                //
+                $stmt_identity = $model->getIdentity();
+                $stmt_email = $model->getContent();
+
+                // Executes the query
+                $stmt->execute();
+
+                // commits the statement
+                $this->getConnector()->finish();
+
+                $model->setIdentity( $stmt->insert_id );
+                $retVal = $model;
+            }
+            catch( Exception $ex )
+            {
+                // Rolls back, the changes
+                $this->getConnector()->undo_state();
+
+                echo $ex;
+                throw new Exception( 'Error:' . $ex );
+            }
+            finally
+            {
+                $this->getConnector()->disconnect();
+            }
+
+            return $retVal;
         }
 
 
