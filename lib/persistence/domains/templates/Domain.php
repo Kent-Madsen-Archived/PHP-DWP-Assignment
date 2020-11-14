8cc7626415c3519051052cced9b95a1a729d551c
Kent vejrup Madsen <Kent.vejrup.madsen@outlook.com>
2020-11-14 02:50:51 +0100

    current

--- lib/persistence/domains/templates/Domain.php
+++ lib/persistence/domains/templates/Domain.php
@@ -5,6 +5,28 @@
      */
     abstract class Domain
     {
+
+        // Variables
+        private $information = null;
+
+        // Accessor
+            // Getter
+        /**
+         * 
+         */
+        final public function getInformation()
+        {
+            return $this->information;
+        }
+
+            // Setter
+        /**
+         * 
+         */
+        final public function setInformation( $var )
+        {
+            $this->information = $var;
+        }
         
     }
 
