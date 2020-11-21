3c445a5d6638d089fbf546c3ae12fe74fdd5909d
Kent vejrup Madsen <Kent.vejrup.madsen@outlook.com>
2020-11-21 17:11:09 +0100

    current

--- lib/persistence/model/templates/DatabaseForm.php
+++ lib/persistence/model/templates/DatabaseForm.php
@@ -5,6 +5,39 @@
      */
     abstract class DatabaseForm
     {
+        // Variable
+        private $isSet = null;
+
+        // Accessors
+            // Getters
+        /**
+         * @return bool|null
+         */
+        final public function getIsSet(): ?bool
+        {
+            return $this->isSet;
+        }
+
+            // Setters
+        /**
+         * @param $isSet
+         * @return bool|null
+         */
+        final protected function setIsSet( $isSet ): ?bool
+        {
+            $this->isSet = $isSet;
+
+            return $this->getIsSet();
+        }
+
+
+        /**
+         *
+         */
+        final public function done()
+        {
+            $this->setIsSet(true );
+        }
 
     }
 
