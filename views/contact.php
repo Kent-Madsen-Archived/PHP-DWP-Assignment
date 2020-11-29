9292091b1254c02dc802706b637284c54a22de19
Kent vejrup Madsen <Kent.vejrup.madsen@outlook.com>
2020-11-29 13:04:05 +0100

    added materialise icons

--- views/contact.php
+++ views/contact.php
@@ -30,6 +30,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <link rel="stylesheet" href="/assets/css/style.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
         <script src='https://www.google.com/recaptcha/api.js' async defer></script>
             
--- views/news.php
+++ views/news.php
@@ -16,6 +16,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <link rel="stylesheet" href="/assets/css/style.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         
         <?php
              PageTitleView::getSingletonView()->printHTML();
