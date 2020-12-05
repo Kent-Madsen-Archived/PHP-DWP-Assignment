4710b5b467a131fb9486347c7d2df962cc620d04
Kent vejrup Madsen <Kent.vejrup.madsen@outlook.com>
2020-12-05 04:36:35 +0100

    can update news articles now

--- views/admin/news/update.php
+++ views/admin/news/update.php
@@ -1,3 +1,40 @@
-<h4>
-    Update
-</h4>
+<?php
+$article_factory = GroupNews::getArticleFactory();
+
+if( isset( $_POST[ 'admin_news_update_submit' ] ) )
+{
+    $identity = trim( $_POST[ 'admin_news_update_identity' ] );
+    $title = trim( $_POST[ 'admin_news_update_title' ] );
+    $content = trim($_POST['admin_news_update_content']);
+
+    $fTitle = filter_var( $title, FILTER_SANITIZE_STRING );
+    $fContent = filter_var( $content, FILTER_SANITIZE_STRING );
+    $filtered_id = filter_var( $identity, FILTER_VALIDATE_INT );
+
+    $m = $article_factory->createModel();
+
+    $m->setIdentity($filtered_id);
+
+    $m->setTitle($fTitle);
+    $m->setContent($fContent);
+
+    $article_factory->update($m);
+    $article_factory->readModel($m);
+
+    echo "updated model at: {$m->getLastUpdated()}";
+}
+?>
+
+<h4>Update</h4>
+<form action="/admin/news/update" method="post">
+    <input type="hidden" value="<?php echo "{$id_value}"; ?>" name="admin_news_update_identity">
+
+    <label>Title</label>
+    <input type="text" value="" name="admin_news_update_title">
+
+    <label>Content</label>
+    <input type="text" value="" name="admin_news_update_content">
+
+    <input class="button" type="submit" value="update article" name="admin_news_update_submit">
+</form>
+
